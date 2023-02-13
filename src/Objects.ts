f35abb8fc179faee3432b41f274894fa8db5c640
EvanilsonP <pevanilson99@gmail.com>
2023-02-13 11:16:16 -0300

    Objects

---
+++ src/Objects.ts
@@ -0,0 +1,11 @@
+// Objects
+// Object is a standalone entity with properties and type
+type User = {
+    id: number,
+    name: string
+};
+
+const user: User = {
+    id: 1,
+    name: 'Evanilson'
+};
