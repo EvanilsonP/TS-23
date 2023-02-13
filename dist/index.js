e04bf86fd511ec313cdf5734d746c566177fdd4f
EvanilsonP <pevanilson99@gmail.com>
2023-02-13 10:42:48 -0300

    Tuple and tuple array

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+// Basic types
+let id = 5;
+let company = 'Traversy media';
+let isPublished = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hi'];
+// Tuple
+let person = [1, 'Evanilson', true];
+// tuple array
+let employee;
+employee = [
+    [1, 'Joel'],
+    [2, 'Tess'],
+    [3, 'Ellie'],
+];
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// Basic types
+let id: number = 5;
+let company: string  = 'Traversy media';
+let isPublished: boolean = true;
+let x: any = 'Hello';
+
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, 'Hi'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Evanilson', true];
+
+// tuple array
+let employee: [number, string][];
+
+employee = [
+    [1, 'Joel'],
+    [2, 'Tess'],
+    [3, 'Ellie'],
+];
